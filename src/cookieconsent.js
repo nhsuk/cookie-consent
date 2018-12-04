5689572780c0e445fceedd41b863a0004702d850
Brad Morton <bradley.morton1@nhs.net>
2018-12-04 10:45:42 +0000

    added javascript injected css/html for banner/modal

--- src/cookieconsent.js
+++ src/cookieconsent.js
@@ -1,9 +1,25 @@
-import { getCookie, createCookie } from './cookies'
+import { getCookie, createCookie, insertCookieBanner } from './cookies'
 
 var delimiter = "---"; //used to split cookie into information
 
+window.onload = function checkCookie() {
+    var cookieName = "visited";
+    //If there isn't a user cookie, create one
+    if (getCookie(cookieName) == null) {
+        var cookieTypes = "necessary:true"+delimiter+"preferences:false"+delimiter+"statistics:false"+delimiter+"marketing:false";
+        createCookie(cookieName, cookieTypes, 365);
+        insertCookieBanner();
+    }
+};
 
 //If consent is given, change value of cookie
 function acceptConsent() {
-    var cookieTypes = "necessary:true"+delimiter+"preferences:false"+delimiter+"statistics:false"+delimiter+"marketing:false";
-}
+    var cookieTypes = "necessary:true"+delimiter+"preferences:true"+delimiter+"statistics:true"+delimiter+"marketing:true";
+    createCookie("visited", cookieTypes, -1);
+};
+
+function hideCookieBanner() {
+    var cookiebanner = document.getElementById("cookiebanner");
+    cookiebanner.style.display = "none";
+};
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/cookieconsent.js',
   output: {
     filename: 'main.js',
