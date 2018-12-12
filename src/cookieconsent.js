e886965bb3b197dc809005f334292ced60f7a651
Brad Morton <bradley.morton1@nhs.net>
2018-12-12 13:59:18 +0000

    added functions to toggle consent, and read consent of cookie

--- src/cookieconsent.js
+++ src/cookieconsent.js
@@ -6,11 +6,18 @@ var delimiter = "---"; //used to split cookie into information
 
 // If cookie rules/regulations change and the cookie itself needs to change, bump this version up afterwards.
 // It will then give the user the banner again to consent to the new rules
-var COOKIE_VERSION = 1;
-var cookieTypes = "necessary:true"+delimiter+"preferences:true"+delimiter+"statistics:true"+delimiter+"marketing:false"+"|"+COOKIE_VERSION;
+export var COOKIE_VERSION = 1;
+var cookieTypes = {
+    "Necessary": true,
+    "Preferences": true,
+    "Statistics": true,
+    "Marketing": false,
+    "Version": COOKIE_VERSION,
+};
 
 window.onload = function checkCookie() {
     var cookieName = "nhsuk-cookie-consent";
+    togglePreferences(cookieName);
     //If there isn't a user cookie, create one
     if (getCookie(cookieName) == null) {
         createCookie(cookieName, cookieTypes, 365, "/");
@@ -23,7 +30,8 @@ window.onload = function checkCookie() {
 
 //If consent is given, change value of cookie
 export function acceptConsent() {
-    var cookieTypesAccepted = "necessary:true"+delimiter+"preferences:true"+delimiter+"statistics:true"+delimiter+"marketing:true"+"|"+COOKIE_VERSION;
+    var cookieTypesAccepted = cookieTypes;
+    cookieTypesAccepted.Marketing = true;
     createCookie("nhsuk-cookie-consent", cookieTypesAccepted, -1, "/");
     createCookie("nhsuk-cookie-consent", cookieTypesAccepted, 365, "/");
     hideCookieModal();
@@ -36,7 +44,7 @@ function getCookieVersion(name) {
 }
 
 function isValidVersion(name, version) {
-    if (getCookieVersion(name) == version)
+    if (getCookieVersion(name) <= version)
         return true;
     else
         return false;
@@ -57,4 +65,37 @@ window.NHSCookieConsent = {
    * The version of this package as defined in the package.json
    */
   VERSION: packageJson.version,
+}
+
+function getPreferences(name) {
+    var cookie = getCookie(name);
+    return cookie.Preferences;
+}
+
+function getStatistics(name) {
+    var cookie = getCookie(name);
+    return cookie.Statistics;
+}
+
+function getMarketing(name) {
+    var cookie = getCookie(name);
+    return cookie.Marketing;
+}
+
+function togglePreferences(name) {
+    var cookie = JSON.parse(getCookie(name));
+    cookie.Preferences = !cookie.Preferences;
+    createCookie(name, JSON.stringify(cookie), 365, "/");
+}
+
+function toggleStatistics(name) {
+    var cookie = JSON.parse(getCookie(name));
+    cookie.Statistics = !cookie.Statistics;
+    createCookie(name, JSON.stringify(cookie), 365, "/");
+}
+
+function toggleMarketing(name) {
+    var cookie = JSON.parse(getCookie(name));
+    cookie.Marketing = !cookie.Marketing;
+    createCookie(name, JSON.stringify(cookie), 365, "/");
 }
--- src/cookieconsent.test.js
+++ src/cookieconsent.test.js
@@ -3,3 +3,7 @@ import { acceptConsent } from './cookieconsent'
 test('acceptConsent function exists', () => {
   expect(acceptConsent).toBeInstanceOf(Function)
 })
+
+test('getPreferences returns correct value', () => {
+  
+})
