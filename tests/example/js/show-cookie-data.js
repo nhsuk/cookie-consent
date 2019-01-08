2893e09d001f72c3d0ee3f15eaa9ee71532e59d1
Mike Monteith <mike@mikemonteith.com>
2019-01-08 11:40:43 +0000

    Refactor duplicated javascript in example pages

---
+++ tests/example/js/show-cookie-data.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-var, prefer-arrow-callback, prefer-template */
+
+/**
+ * function for displaying cookie information on the example pages.
+ * This isn't as nice as the chrome dev tools panel, but it's something.
+ *
+ * Looks for an element with id="cookie-list" to insert cookie data into.
+ * Looks for an element with id="cookie-list-refresh" to use as a refresh button.
+ */
+(function showCookieData() {
+  var container = document.getElementById('cookie-list');
+  var refreshButton = document.getElementById('cookie-list-refresh');
+
+  function listCookies() {
+    // Get all cookies and render them in the cookie-list div
+    var cookies = document.cookie ? document.cookie.split(';') : [];
+
+    // set the cookie list
+    container.innerHTML = '';
+    if (cookies.length) {
+      cookies.forEach(function forEachCookie(cookie) {
+        var lineItem = '<li>' + cookie + '</li>';
+        container.insertAdjacentHTML('beforeend', lineItem);
+      });
+    } else {
+      container.innerHTML = '<p>No cookies to show</p>';
+    }
+  }
+
+  refreshButton.addEventListener('click', listCookies);
+  listCookies();
+}());
