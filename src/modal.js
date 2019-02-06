54af41fb8215be065af3de04cbd85131a97330a3
Brad Morton <bradley.morton1@nhs.net>
2019-02-06 07:55:57 +0000

    added karls new design iteration

--- src/modal.js
+++ src/modal.js
@@ -10,10 +10,10 @@ export function hideCookieModal() {
 export function insertCookieBanner(acceptConsent) {
   // add a css block to the inserted html
   const html = `${modalHtml} <style>${modalCss.toString()}</style>`;
-  document.getElementsByTagName('body')[0].innerHTML += html;
+  let bod = document.getElementsByTagName('body')[0].innerHTML;
+  bod = html + bod;
 
-  document.getElementsByClassName('nhsuk-cookie-banner__link_accept')[0].addEventListener('click', acceptConsent);
-
+  document.getElementById('nhsuk-cookie-banner__link_accept').addEventListener('click', acceptConsent);
   const cookieBannerHeight = document.getElementById('cookiebanner').offsetHeight;
   const header = document.getElementsByClassName('nhsuk-global-header')[0];
   header.style.marginTop = cookieBannerHeight.toString() + 'px';
