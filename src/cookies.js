720263baa8ee85b2d359603c316e51177b176b30
Brad Morton <bradley.morton1@nhs.net>
2019-03-04 12:58:48 +0000

    removed duplicate create cookie function

--- src/cookies.js
+++ src/cookies.js
@@ -37,39 +37,6 @@ export function createCookie(name, value, days, path, domain, secure) {
   document.cookie = cookieString;
 }
 
-//used to create a new cookie for the user which covers different cookie types
-export function createCookie(name, value, days, path, domain, secure){
-    //if number of days is given, sets expiry time
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
-        var expires=date.toGMTString();
-    }
-    else {
-        var expires = "";
-    }
-
-    //appends name to cookie, making it searchable
-    var cookieString= name + "=" + escape (value);
-
-    if (expires)
-    cookieString += ";expires=" + expires;
-
-    if (path)
-    cookieString += ";path=" + escape (path);
-
-    if (domain)
-    cookieString += ";domain=" + escape (domain);
-
-    if (secure)
-    cookieString += ";secure" + escape (secure);
-
-    cookieString += ";";
-
-    //cookiestring now contains all necessary details and is turned into a cookie
-    document.cookie=cookieString;
-};
-
 // gets a cookie based on the name
 export function getCookie(name) {
   var getCookieValues = function(cookie) {
