b2e214ba0648557e5f18de14736046d2a8b10009
Mike Monteith <mike@mikemonteith.com>
2019-03-04 16:58:01 +0000

    add more banner integration tests

--- tests/integration-tests/banner.test.js
+++ tests/integration-tests/banner.test.js
@@ -2,19 +2,19 @@
 
 import { clearAllCookies } from './util';
 
+const waitForVisibleBanner = async () => {
+  await page.waitForSelector('.nhsuk-cookie-banner', { visible: true });
+};
+
+const waitForHiddenBanner = async () => {
+  await page.waitForSelector('.nhsuk-cookie-banner', { hidden: true });
+};
+
 describe('Banner is usable', () => {
-  const waitForVisibleModal = async () => {
-    await page.waitForSelector('.nhsuk-cookie-banner', { visible: true });
-  };
-
-  const waitForHiddenModal = async () => {
-    await page.waitForSelector('.nhsuk-cookie-banner', { hidden: true });
-  };
-
   beforeEach(async () => {
     await clearAllCookies();
     await page.goto('http://localhost:8080/tests/example/');
-    await waitForVisibleModal();
+    await waitForVisibleBanner();
   });
 
   it('should display on first page load', async () => {
@@ -23,7 +23,18 @@ describe('Banner is usable', () => {
 
   it('clicking the accept button should hide banner', async () => {
     await page.click('#nhsuk-cookie-banner__link_accept');
-    await waitForHiddenModal();
+    await waitForHiddenBanner();
+  });
+
+  it('clicking the accept button should show confirmation banner', async () => {
+    await page.click('#nhsuk-cookie-banner__link_accept');
+    await waitForHiddenBanner();
+    await page.waitForSelector('#nhsuk-cookie-confirmation-banner', { visible: true });
+  });
+
+  it('clicking the accept buttons should keep user on the same page', async () => {
+    await page.click('#nhsuk-cookie-banner__link_accept');
+    expect(page.url()).toEqual('http://localhost:8080/tests/example/');
   });
 
   it('clicking "change cookie settings" should take the user to another page', async () => {
@@ -31,6 +42,35 @@ describe('Banner is usable', () => {
       page.waitForRequest('http://localhost:8080/our-policies/cookies-policy'),
       page.click('#nhsuk-cookie-banner__link'),
     ]);
+  });
+});
+
+describe('Remember cookie state', () => {
+  beforeEach(async () => {
+    await clearAllCookies();
+    await page.goto('http://localhost:8080/tests/example/');
+    await waitForVisibleBanner();
+  });
+
+  it('clicking accept button should prevent banner if revisited', async () => {
+    await page.click('#nhsuk-cookie-banner__link_accept');
+    await waitForHiddenBanner();
+    await page.goto('http://localhost:8080/tests/example/');
+    const banner = await page.evaluate(async () => document.querySelector('.nhsuk-cookie-banner'));
+    expect(banner).toBe(null);
+  });
+
+  it('not accepting should show the banner again if revisited', async () => {
+    await page.goto('http://localhost:8080/tests/example/');
+    const banner = await page.evaluate(async () => document.querySelector('.nhsuk-cookie-banner'));
+    expect(banner).not.toBe(null);
+  });
+
+  it('going to "change cookie settings" should not show the banner again if revisited', async () => {
+    await page.click('#nhsuk-cookie-banner__link');
+    await page.goto('http://localhost:8080/tests/example/');
+    const banner = await page.evaluate(async () => document.querySelector('.nhsuk-cookie-banner'));
+    expect(banner).toBe(null);
   });
 });
 
