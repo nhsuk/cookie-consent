1f4cf2d12089ff2bf402a58cf7acb092f0151899
Brad Morton <bradley.morton1@nhs.net>
2019-03-05 09:50:08 +0000

    changed references to modal in favour of new banner

--- tests/integration-tests/modal.test.js
+++ tests/integration-tests/modal.test.js
@@ -2,37 +2,37 @@ import { clearAllCookies } from './util'
 
 describe('Popup is usable', () => {
 
-  const waitForVisibleModal = async () => {
+  const waitForVisibleBanner = async () => {
     await page.waitForSelector('.nhsuk-cookie-banner', { visible: true })
   }
 
-  const waitForHiddenModal = async () => {
+  const waitForHiddenBanner = async () => {
     await page.waitForSelector('.nhsuk-cookie-banner', { hidden: true })
   }
 
   beforeEach(async () => {
     await clearAllCookies()
     await page.goto('http://localhost:8080/tests/example/')
-    await waitForVisibleModal()
+    await waitForVisibleBanner()
   })
 
   it('should display on first page load', async () => {
     await expect(page).toMatch("Cookies on our website")
   })
 
-  it('clicking the accept button should hide modal', async () => {
+  it('clicking the accept button should hide banner', async () => {
     await page.click('.nhsuk-cookie-banner button')
-    await waitForHiddenModal()
+    await waitForHiddenBanner()
   })
 
-  it('clicking outside of the modal should hide modal', async () => {
+  it('clicking outside of the banner should hide banner', async () => {
     // Click the cookie banner background with javascript in-browser.
     // for some reason, page.click('.nhsuk-cookie-banner') throws a pupeteer error
     // TypeError: Cannot read property 'addExpectationResult' of undefined
     await page.evaluate(async () => {
       document.querySelector('.nhsuk-cookie-banner').click()
     })
-    await waitForHiddenModal()
+    await waitForHiddenBanner()
   })
 
   it('clicking "tell me more" should take the user to another page', async () => {
@@ -42,9 +42,9 @@ describe('Popup is usable', () => {
     ])
   })
 
-  it('clicking "ask me later" should hide modal', async () => {
+  it('clicking "ask me later" should hide banner', async () => {
     await page.click('#later-link')
-    await waitForHiddenModal()
+    await waitForHiddenBanner()
   })
 
 })
