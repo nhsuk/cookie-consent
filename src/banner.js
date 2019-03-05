919699a61681d1ff4f3f3b4dd2f396e931291156
Brad Morton <bradley.morton1@nhs.net>
2019-03-05 15:04:14 +0000

    quick and dirty solution for service manual

--- src/banner.js
+++ src/banner.js
@@ -1,28 +1,81 @@
 import bannerHtml from './banner.html';
 import bannerCss from './style.css';
-import { toggleConsented as consent } from './cookieconsent';
 
 export function hideCookieBanner() {
-  document.getElementById('cookiebanner').style.display = 'none';
+  document.getElementById('cookie-banner').style.display = 'none';
 }
 
-export function showCookieConfirmation() {
-  document.getElementById('nhsuk-cookie-confirmation-banner').style.display = 'block';
-}
-
-function goToCookieSettings() {
-  consent();
-  hideCookieBanner();
-}
-
-export function insertCookieBanner(acceptConsent) {
+export function insertCookieBanner() {
   // add a css block to the inserted html
   const html = `${bannerHtml} <style>${bannerCss.toString()}</style>`;
   document.getElementsByTagName('body')[0].innerHTML = html + document.getElementsByTagName('body')[0].innerHTML;
-
-  document.getElementById('nhsuk-cookie-banner__link_accept').addEventListener('click', (e) => {
-    e.preventDefault();
-    acceptConsent();
-  });
-  document.getElementById('nhsuk-cookie-banner__link').addEventListener('click', goToCookieSettings);
+  document.getElementById('cookiebannerclose').onclick = hideCookieBanner;
 }
+
+const COOKIE_NAME = 'nhsuk-cookie-consent';
+
+// gets a raw cookie from list of cookies based on name
+export function getCookie(name) {
+  var getCookieValues = function(cookie) {
+    if (document.cookie != "") {
+      var cookieArray = cookie.split('=');
+      return cookieArray[1].trim();
+    }
+  };
+
+  var getCookieNames = function(cookie) {
+	var cookieArray = cookie.split('=');
+    return cookieArray[0].trim();
+  };
+
+  var cookies = document.cookie.split(';');
+  var cookieValue = cookies.map(getCookieValues)[cookies.map(getCookieNames).indexOf(name)];
+  return (cookieValue === undefined) ? null : decodeURIComponent(cookieValue);
+}
+
+export function createCookie(name, value, days, path, domain, secure) {
+  // if number of days is given, sets expiry time
+  let expires;
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = date.toGMTString();
+  } else {
+    expires = '';
+  }
+
+  // appends name to cookie, making it searchable
+  let cookieString = `${name}=${escape(value)}`;
+
+  if (expires) {
+    cookieString += ';expires=' + expires;
+  }
+
+  if (path) {
+    cookieString += ';path=' + escape(path);
+  }
+
+  if (domain) {
+    cookieString += ';domain=' + escape(domain);
+  }
+
+  if (secure) {
+    cookieString += ';secure';
+  }
+
+  cookieString += ';';
+
+  // cookiestring now contains all necessary details and is turned into a cookie
+  document.cookie = cookieString;
+}
+
+function checkCookie() {
+  // If there isn't a user cookie, create one
+  console.log(getCookie(COOKIE_NAME));
+  if (getCookie(COOKIE_NAME) == null) {
+    createCookie(COOKIE_NAME, COOKIE_NAME, 365, '/');
+    insertCookieBanner();
+  }
+}
+
+window.addEventListener('load', checkCookie);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/cookieconsent.js',
+  entry: './src/banner.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
