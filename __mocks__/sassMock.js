bf0b8fbc7f528ef4e8066f7269a50c84730af1ca
Mike Monteith <mike@mikemonteith.com>
2019-03-06 09:09:49 +0000

    Mock to let jest handle scss imports

--- __mocks__/sassMock.js
+++ __mocks__/sassMock.js
@@ -1,5 +1,5 @@
-// Mock for the css-loader.
-// In jest unit tests, all import ... from 'style.css' statements
+// Mock for the sass-loader.
+// In jest unit tests, all import ... from 'style.scss' statements
 // will return an object with a toString() method that returns a css string
 
 module.exports = {
