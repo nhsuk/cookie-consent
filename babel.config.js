e89edc6209197b9cb94f392cea723df33357d7fc
Mike Monteith <mike@mikemonteith.com>
2019-04-01 19:53:03 +0100

    Make babel config relevant to the run environment
    - Only enable rewire in test runs
    - Only compile for node in test runs
    
    This change reduces the build by around 10kb.

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,29 @@
-module.exports = {
-  plugins: ['babel-plugin-rewire'],
-  presets: [
-    ['@babel/preset-env', {
-      targets: {
-        node: 'current',
-      },
-    }],
-  ],
+module.exports = (api) => {
+  const isTest = api.env('test');
+
+  let targets = {};
+  let plugins = [];
+
+  if (isTest) {
+    // Add node to babel targets when running tests
+    targets = {
+      ...targets,
+      node: 'current',
+    };
+
+    // Add rewire for jest unit tests
+    plugins = [
+      ...plugins,
+      'babel-plugin-rewire',
+    ];
+  }
+
+  const config = {
+    plugins,
+    presets: [
+      ['@babel/preset-env', { targets }],
+    ],
+  };
+
+  return config;
 };
